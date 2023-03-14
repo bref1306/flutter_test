aa3b546b1679034094496a9c5bee298cb2456f03
Auriane <auriane@uccelolabs.com>
2023-03-14 10:13:25 +0100

    feat: group test

--- test/maths_utils_test.dart
+++ test/maths_utils_test.dart
@@ -2,15 +2,41 @@ import 'package:app_test/maths_utils.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test("check for 2 numbers addition", () {
-    // Arrange
-    int a = 10;
-    int b = 10;
+  group('test Maths Utils', () { 
+     test("check for 2 numbers addition", () {
+      // Arrange
+      int a = 10;
+      int b = 10;
 
-    // Act
-    var sum = add(a, b);
+      // Act
+      var sum = add(a, b);
 
-    // Assert
-    expect(sum, 20);
+      // Assert
+      expect(sum, 20);
+    });  
+
+    test("check for 2 numbers substraction", () {
+      // Arrange
+      int a = 10;
+      int b = 10;
+
+      // Act
+      var sum = substract(a, b);
+
+      // Assert
+      expect(sum, 20);
+    });  
+
+    test("check for 2 numbers multiply", () {
+      // Arrange
+      int a = 10;
+      int b = 10;
+
+      // Act
+      var sum = substract(a, b);
+
+      // Assert
+      expect(sum, 20);
+    });
   });  
 }
