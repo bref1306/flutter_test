b6d82fdb7f59cea44658e9974116d3d069f3754c
Auriane <auriane@uccelolabs.com>
2023-08-28 14:14:25 +0200

    feat: add test empty field

--- integration_test/login_flow_test.dart
+++ integration_test/login_flow_test.dart
@@ -6,6 +6,26 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   group("Login Flow Test", () { 
     IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+      testWidgets('Should show Required Fields Error message when user taps on login button without entering email id & password', (WidgetTester tester) async {
+      //Arrange
+        await tester.pumpWidget(
+          const MaterialApp(
+            home: LoginScreen()
+          )
+        );
+
+        //Act    
+        Finder loginButton = find.byKey(const Key('login_button')); 
+        await tester.tap(loginButton);
+        await tester.pumpAndSettle(const Duration(seconds: 2)); 
+
+          // On vérifie la présence d'un texte pour vérifier que le message d'erreur apparaît bien
+        Finder errorText = find.text("Required Field"); 
+
+        // Assert
+          // On vérifie qu'il y a bien exactement 2 champs qui doivent faire apparaître le message d'erreur
+        expect(errorText, findsNWidgets(2)); 
+    });
       testWidgets('Should show home screen on click of login button when user entered valid email id & password', (WidgetTester tester) async {
       //Arrange
         await tester.pumpWidget(
@@ -23,7 +43,7 @@ void main() {
           
         Finder loginButton = find.byKey(const Key('login_button')); 
         await tester.tap(loginButton);
-        await tester.pumpAndSettle(); 
+        await tester.pumpAndSettle(const Duration(seconds: 2)); 
 
           // On vérifie la présence d'un texte pour signifier que nous sommes bien redirigés vers HomeScreen() après l'évènement du clic
         Finder welcomeText = find.byType(Text); 
